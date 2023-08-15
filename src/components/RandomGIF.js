930cb99198236d3c62e153675023cd7cefaa09b3
Manash Roy <manashroy454@gmail.com>
2023-08-15 11:19:59 +0530

    Updated

--- src/components/RandomGIF.js
+++ src/components/RandomGIF.js
@@ -7,16 +7,16 @@ function Random(){
 
     return(
         <div className='w-1/2  random-gif-bg rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]'>
-            <h1 className='mt-[15px] text-2xl underline uppercase font-bold'> 
+            <h1 className='gif-heading mt-[15px] text-2xl underline uppercase font-bold'> 
                 Random GIF
             </h1>
 
             {
-                loading ? (<Spinner/>) : <img src={gif} width="450" className='border-double border-8 border-black'/>
+                loading ? (<Spinner/>) : <img src={gif} width="450" className='gif-box border-double border-8 border-black'/>
             }
             {/* If loading is true then show the Spinner otherwise Show the GIF */}
 
-            <button className="button w-7/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]"
+            <button className="button w-7/12 bg-yellow-500 text-lg  rounded-lg mb-[30px]"
                 onClick={() => fetchData()}>
                 Generate
             </button>
--- src/components/SearchGIF.js
+++ src/components/SearchGIF.js
@@ -12,20 +12,20 @@ function SearchGIF(){
 
     return(
         <div className='w-1/2  srch-gif-bg rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]'>
-            <h1 className='mt-[15px] text-2xl underline uppercase font-bold'>
+            <h1 className='gif-heading mt-[15px] text-2xl underline uppercase font-bold'>
                 Random {searchWord} Gif
             </h1>
 
             {
-                loading ? (<Spinner/>) : <img src={gif} width="450" className='border-double border-8 border-black'/>
+                loading ? (<Spinner/>) : <img src={gif} width="450" className='gif-box border-double border-8 border-black'/>
             }
             {/* If loading is true then show the Spinner otherwise Show the GIF */}
 
-            <input className='w-8/12 text-lg py-2 rounded-lg mb-[3px] text-center'
+            <input className='search-bar w-8/12 text-lg py-2 rounded-lg mb-[3px] text-center'
                 placeholder="Search GIF here" 
                 onChange={changeHandler} />
 
-            <button className="button w-7/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]"
+            <button className="button w-7/12 bg-yellow-500 text-lg  rounded-lg mb-[30px]"
                 onClick={() => fetchData(searchWord)}>
                 Generate
             </button>
